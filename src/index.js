d840de66280a934f605bbfdb3c5e3dc692940393
Paul Kasidit <paulkasidit@gmail.com>
2022-08-12 14:15:38 -0700

    working on UI and business logic

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//UI Logic 
+
+
+//Business Logic  
---
+++ src/js/exchangeAPI.js
@@ -0,0 +1,19 @@
+export default class CurrencyExchange{
+
+  static async getRatesForUSD(){
+    try{
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if(!response.ok){
+        throw Error(response.statusText);
+      }
+      return await response.json();
+    } catch(error) {
+      return error.message; //not showing in the dom because user has no interaction with this call
+    }
+
+  
+  
+
+  }
+
+
