f5058379cc4b90f28b63a9ea3cb66ba20c97a4d4
Paul Kasidit <paulkasidit@gmail.com>
2022-08-12 15:34:38 -0700

    remove tests directory

---
+++ src/js/exchangeLogic.js
@@ -0,0 +1,10 @@
+export default class exchangeProfile {  
+
+  constructor(time) {  
+    this.date = time; 
+    this.usdAmountRequestedToBeConverted = 0; 
+    this.targetCurrency = ''; 
+    this.currentExchangeRate = 0; 
+  }
+
+}
